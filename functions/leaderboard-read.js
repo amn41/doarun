89be0f92960f9da1dd0098157637074914d2db9e
Alan Nichol <alan@rasa.com>
2021-02-27 21:27:30 +0100

    update leaderboard fn

--- functions/leaderboard-read.js
+++ functions/leaderboard-read.js
@@ -2,12 +2,15 @@
 const faunadb = require('faunadb')
 const q = faunadb.query
 
-
-exports.handler = (event, context) => {
+exports.handler = function (event, context, callback) {
   console.log('Function `leaderboard-read` invoked')
-    return [
+  const leaderboard = [
 	{"name": "Alan Nichol", "points": 3},
 	{"name": "Brian Daly", "points": 1},
 	{"name": "Shit Muncher", "points": 0}
-    ]
-}
+  ]
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(leaderboard),
+  });
+};
