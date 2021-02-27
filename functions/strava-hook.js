93f8ebf78ec25340b80b9919a159c426264f9731
Alan Nichol <alan@rasa.com>
2021-02-27 15:56:24 +0100

    access with []

--- functions/strava-hook.js
+++ functions/strava-hook.js
@@ -1,6 +1,6 @@
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({"hub.challenge":event.queryStringParameters.hub.challenge})
+        body: JSON.stringify({"hub.challenge":event.queryStringParameters["hub.challenge"]})
     };
 }
