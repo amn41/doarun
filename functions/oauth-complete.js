416886b39179193e26912a1d47b4e0cbef84a727
Alan Nichol <alan@rasa.com>
2021-02-28 16:34:02 +0100

    finish oauth fun

---
+++ functions/oauth-complete.js
@@ -0,0 +1,22 @@
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+const page = '<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>DOARUN</title>
+  </head>
+  <body>
+    <h1>hi</h1>
+  </body>
+</html>'
+
+exports.handler = function (event, context, callback) {
+  console.log('Function `oauth-complete` invoked')
+  callback(null, {
+    statusCode: 200,
+    body: page,
+  });
+};
