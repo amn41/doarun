88091e05b723373dcbd3e0c009376bc9a3a07884
Alan Nichol <alan@rasa.com>
2021-02-28 13:29:23 +0100

    add profile read function

---
+++ functions/profile-read.js
@@ -0,0 +1,9 @@
+
+exports.handler = (event, context, callback) => {
+  const {identity, user} = context.clientContext;
+  const data = { "identity": identity, "user": user }  
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  });
+}
