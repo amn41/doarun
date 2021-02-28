1a56176b42035bba936ec695aecc4cdf83ccd24f
Alan Nichol <alan@rasa.com>
2021-02-28 16:36:49 +0100

    singel line

--- functions/oauth-complete.js
+++ functions/oauth-complete.js
@@ -2,16 +2,7 @@
 const faunadb = require('faunadb')
 const q = faunadb.query
 
-const page = '<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>DOARUN</title>
-  </head>
-  <body>
-    <h1>hi</h1>
-  </body>
-</html>'
+const page = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>DOARUN</title></head><body><h1>hi</h1></body></html>'
 
 exports.handler = function (event, context, callback) {
   console.log('Function `oauth-complete` invoked')
