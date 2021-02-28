f868542f0cde397f8ddf2086f15432ec338e7bf2
Alan Nichol <alan@rasa.com>
2021-02-28 20:04:15 +0100

    profile pic with link

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../../assets/doarun-logo.svg'
 import styles from './AppHeader.css' // eslint-disable-line
+import { Link } from 'react-router-dom';
 
 const AppHeader = (props) => {
   return (
@@ -10,6 +11,11 @@ const AppHeader = (props) => {
           <div className='app-left-nav'>
             <img src={logo} className='app-logo' alt='logo' />
           </div>
+        </div>
+        <div className='app-right-nav'>
+          <Link to="/profile">
+            <img alt="profile" className="avatar" title="View Profile" src="https://dgalywyr863hv.cloudfront.net/pictures/athletes/296399/461322/2/medium.jpg"/>
+          </Link>
         </div>
       </div>
     </header>
