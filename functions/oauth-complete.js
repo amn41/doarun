a8b9605ed4d340e95dae2ca0800d575a7122584a
Alan Nichol <alan@rasa.com>
2021-02-28 16:42:08 +0100

    script with redirect

--- functions/oauth-complete.js
+++ functions/oauth-complete.js
@@ -2,7 +2,20 @@
 const faunadb = require('faunadb')
 const q = faunadb.query
 
-const page = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>DOARUN</title></head><body><h1>hi</h1></body></html>'
+const page = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>DOARUN</title>
+</head>
+<body>
+  <h3>authentication successful</h3>
+</body>
+<script type="text/javascript">
+  window.location.replace("https://amazing-jang-8a41ee.netlify.app");
+</script>
+</html>`
 
 exports.handler = function (event, context, callback) {
   console.log('Function `oauth-complete` invoked')
