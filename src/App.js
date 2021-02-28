7da31e1d8d343d539cd5168aefcd65636df41f1d
Alan Nichol <alan@rasa.com>
2021-02-28 12:56:00 +0100

    login

--- src/App.js
+++ src/App.js
@@ -3,135 +3,119 @@ import AppHeader from './components/AppHeader'
 import api from './utils/api'
 import isLocalHost from './utils/isLocalHost'
 import './App.css'
+import Leaderboard from './Leaderboard';
+import netlifyIdentity from 'netlify-identity-widget';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
+
+
+const netlifyAuth = {
+  isAuthenticated: false,
+  user: null,
+  authenticate(callback) {
+    this.isAuthenticated = true;
+    netlifyIdentity.open();
+    netlifyIdentity.on('login', user => {
+      this.user = user;
+      callback(user);
+    });
+  },
+  signout(callback) {
+    this.isAuthenticated = false;
+    netlifyIdentity.logout();
+    netlifyIdentity.on('logout', () => {
+      this.user = null;
+      callback();
+    });
+  }
+};
+
+const AuthButton = withRouter(
+  ({ history }) =>
+    netlifyAuth.isAuthenticated ? (
+      <p>
+        Welcome!{' '}
+        <button
+          onClick={() => {
+            netlifyAuth.signout(() => history.push('/'));
+          }}
+        >
+          Sign out
+        </button>
+      </p>
+    ) : (
+      <p>You are not logged in.</p>
+    )
+);
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        netlifyAuth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+class Login extends React.Component {
+  state = { redirectToReferrer: false };
+
+  login = () => {
+    netlifyAuth.authenticate(() => {
+      this.setState({ redirectToReferrer: true });
+    });
+  };
+
+  render() {
+    let { from } = this.props.location.state || { from: { pathname: '/' } };
+    let { redirectToReferrer } = this.state;
+
+    if (redirectToReferrer) return <Redirect to={from} />;
+
+    return (
+      <div>
+        <p>You must log in to view the page at {from.pathname}</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    );
+  }
+}
+
 
 export default class App extends Component {
-  state = {
-      latestActivity: {
-        "athlete": "Alan Nichol",
-        "distance": 1000,
-        "id": 4861036852,
-        "start_date": "2021-02-27T18:50:00Z",
-        "start_date_local": "2021-02-27T18:50:00Z",
-      },
-      weeklyLeaderboard: [{
-	"name": "Alan Nichol",
-	"distance": 15.6,
-	"city": "Berlin"
-      }, {
-	"name": "Brian Daly",
-	"distance": 12.8,
-	"city": "Berlin"
-      }, {
-	"name": "Suzi B",
-	"distance": 11.5,
-	"city": "Berlin"
-      }],
-      targetDistance: 12
-  }
-  componentDidMount() {
-
-    // Fetch data
-    api.readLatestActivity().then((latestActivity) => {
-      if (latestActivity.message === 'unauthorized') {
-        if (isLocalHost()) {
-          alert('FaunaDB key is not authorized. Make sure you set it in terminal session where you ran `npm start`. Visit http://bit.ly/set-fauna-key for more info')
-        } else {
-          alert('FaunaDB key is not authorized. Verify the key `FAUNADB_SERVER_SECRET` set in Netlify enviroment variables is correct')
-        }
-        return false
-      }
-
-      console.log('latest activity', latestActivity)
-      this.setState({
-        latestActivity: latestActivity
-      })
-    })
-    api.readWeeklyLeaderboard().then((leaderboard) => {
-      console.log('weeklyleaderboard', leaderboard)
-      this.setState({
-        weeklyLeaderboard: leaderboard
-      })
-    })
-  }
-  renderDistance(distance) {
-    return ((distance).toString() + " KM")
-  }
-  renderLatestActivity() {
-    const { latestActivity } = this.state
-    if (!latestActivity) {
-      // Loading State here
-      return null
-    }
-      return (
-      <div>
-         {this.renderDistance(latestActivity.distance/1000)}
-      </div>
-      )
-  }
-  renderWeeklyLeaderboardTable() {
-    const { weeklyLeaderboard } = this.state
-
-    if (!weeklyLeaderboard || !weeklyLeaderboard.length) {
-      // Loading State here
-      return null
-    }
-
-      return weeklyLeaderboard.map((athlete, index) => {
-	  const position = (index + 1).toString() + "."
-       
-       return (
-         <tr key={index}>
-            <td><span className="position">{position}</span></td>
-            <td><img alt="profile" className="avatar" src="https://dgalywyr863hv.cloudfront.net/pictures/athletes/296399/461322/2/medium.jpg"/></td>
-            <td>
-	       <p className="athlete-name">{athlete.name}</p>
-	       <p className="athlete-distance">{this.renderDistance(athlete.distance)}</p>
-            </td>
-         </tr>
-       )
-    })
-  }
-  renderPaceMaker() {
-      const { targetDistance } = this.state
-      return (	
-         <tr >
-            <td></td>
-            <td><span className="target-distance">{targetDistance}</span></td>
-            <td>
-               <h1>TARGET DISTANCE</h1>
-            </td>
-         </tr>  
-      )
-  }
-  renderWeeklyLeaderboard() {
-    return (
-      <table id='weekly-leaderboard'>
-        <tbody>
-          {this.renderWeeklyLeaderboardTable()}
-  	  {this.renderPaceMaker()}
-        </tbody>
-      </table>
-    )
-  }	
   render() {
-    return (
-      <div className='app'>
-        <AppHeader />
-	<div className='container'>    
-	  <div className="item weekly-leaderboard-section">
-	    <h1 classname='weekly-leaderboard-title'>THIS WEEK</h1> 
-            <div className='leaderboard-list'>
-              {this.renderWeeklyLeaderboard()}
-            </div>
-          </div>
-          <div className="item latest-run-section">
-            <h1 classname='leaderboard-title'>LATEST RUN: BRIAN</h1> 
-            <div className='activity-list'>
-              {this.renderLatestActivity()}
-            </div>
-          </div>
-        </div>
-      </div>
+      return (
+    	<Router>
+    	  <div>
+    	    <AuthButton />
+    	    <ul>
+    	      <li>
+    	        <Link to="/login">Login Screen</Link>
+    	      </li>
+    	      <li>
+    	        <Link to="/leaderboard">Leaderboard</Link>
+    	      </li>
+    	    </ul>
+    	    <Route path="/login" component={Login} />
+    	    <PrivateRoute path="/leaderboard" component={Leaderboard} />
+    	  </div>
+    	</Router>
     )
   }
 }
