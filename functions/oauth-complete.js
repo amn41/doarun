96a110e61a4da3bbcf7b2797c9c6f4b61cae7e62
Alan Nichol <alan@rasa.com>
2021-02-28 17:00:08 +0100

    try and fetch from strava

--- functions/oauth-complete.js
+++ functions/oauth-complete.js
@@ -1,6 +1,7 @@
 /* Import faunaDB sdk */
 const faunadb = require('faunadb')
 const q = faunadb.query
+const stravaApi = require('strava-v3')
 
 const page = `
 <!DOCTYPE html>
@@ -13,14 +14,21 @@ const page = `
   <h3>authentication successful</h3>
 </body>
 <script type="text/javascript">
-  window.location.replace("https://amazing-jang-8a41ee.netlify.app");
+  //window.location.replace("https://amazing-jang-8a41ee.netlify.app");
 </script>
 </html>`
 
 exports.handler = function (event, context, callback) {
   console.log('Function `oauth-complete` invoked')
-  console.log(event.queryStringParameters.code)
-  console.log(event.queryStringParameters.scope)
+  const code = event.queryStringParameters.code
+  //console.log(event.queryStringParameters.scope)
+  const response = await stravaApi.oauth.getToken(code)
+  console.log("got response from requesting token")
+  console.log(response.status)
+  data = response.json()
+  console.log(JSON.stringify(data))
+  //strava = new stravaApi.client(access_token);
+  //const payload = await strava.athlete.get({})
   callback(null, {
     statusCode: 200,
     body: page,
