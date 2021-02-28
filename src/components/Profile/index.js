ebe37a2076b0f4d2e3c7d8ba86e81b6c676d68af
Alan Nichol <alan@rasa.com>
2021-02-28 18:05:34 +0100

    update redirect url

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import stravaButton from '../../assets/btn_strava_connectwith_orange.svg'
 
-const oauthUrl = "https://www.strava.com/oauth/authorize?client_id=62285&response_type=code&redirect_uri=https://amazing-jang-8a41ee.netlify.app/profile&approval_prompt=force&scope=activity:read_all"
+const oauthUrl = "https://www.strava.com/oauth/authorize?client_id=62285&response_type=code&redirect_uri=https://amazing-jang-8a41ee.netlify.app/.netlify/functions/oauth-complete&approval_prompt=force&scope=activity:read_all"
+
+
+//exchange_token?state=&code=a4039294d0f6534e9bd97fa75e2f7368d3329868&scope=read,activity:read_all
+
 
 export default class Profile extends Component {
   componentDidMount() {
