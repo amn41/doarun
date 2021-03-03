cdeaaee976e4f1f1cb9e044782467d4d02aed7d1
Alan Nichol <alan@rasa.com>
2021-03-03 22:44:33 +0100

    rm

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import AppHeader from './components/AppHeader'
 import api from './utils/api'
-import isLocalHost from './utils/isLocalHost'
 import './App.css'
 
 export default class Leaderboard extends Component {
