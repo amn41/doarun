df9659d58737561696ebdaf60d26335d5a3c6a9d
Alan Nichol <alan@rasa.com>
2021-03-04 22:13:39 +0100

    refactor hook

--- functions/strava-hook.js
+++ functions/strava-hook.js
@@ -18,19 +18,45 @@ exports.handler = async function(event, context) {
           data: data
         }
         client.query(q.Create(q.Ref('classes/stravaevents'), eventItem))
-        strava = new stravaApi.client("")
-        console.log("fetching activity", data.object_id)
-        strava.activities.get(data.object_id)
-        .then((activity) => { 
-           console.log("fetched activity", activity)
-           return {
-             statusCode: 200,
-             body: "ok"
-           }
+        client.query(q.Get(q.Ref('classes/stravaauths/291790587532673540')
+        .then((response) => {
+           console.log('success', response)
+           const refreshToken = responses.data.refresh_token
+           console.log('refresh token', refreshToken)
+           const strava = new stravaApi.client("")
+           return strava.oauth.refreshToken(refreshToken)
         })
-          /* 		   
+        .then((response) => {
+           console.log(response)
+           const { access_token, refresh_token } = response
+           console.log("new access token", access_token)
+           client.query(q.Update('classes/stravaauths/291790587532673540', 
+                                 {data: { access_token: access_token, refresh_token: refresh_token }}
+                        ))
+           return new stravaApi.client(access_token)
+        })
+        .then((strava) => {
+          console.log("fetching activity", data.object_id)
+          strava.activities.get(data.object_id)
+        })
+        .then((activity) => { 
+          console.log("fetched activity", activity)
           const activityItem = { data : activity }
-	  client.query(q.Create(q.Ref('classes/activities'), activityItem)) */
+	  client.query(q.Create(q.Ref('classes/activities'), activityItem))
+             return {
+               statusCode: 200,
+               body: "ok"
+             }
+          })
+        })
+        /*
+	export interface RefreshTokenResponse {
+	    token_type: string;
+	    access_token: string;
+	    expires_at: number;
+	    expires_in: number;
+	    refresh_token: string;
+        */           
         .catch((error) => {
            console.log('error', error)
            return {
