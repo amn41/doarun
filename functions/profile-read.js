c58f558827ebbcf6e3b62e32a6e1c8198596c473
Alan Nichol <alan@rasa.com>
2021-03-07 12:10:00 +0100

    profile read checks strava

--- functions/profile-read.js
+++ functions/profile-read.js
@@ -1,11 +1,23 @@
+const faunadb = require('faunadb')
+const q = faunadb.query
 
-exports.handler = (event, context, callback) => {
+exports.handler = (event, context) => {
   const {identity, user} = context.clientContext;
   const data = { "identity": identity, "user": user }  
   console.log("identity", identity)
   console.log("user", user)
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(data),
-  });
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET
+  }) 
+  return client.query(
+    q.Paginate(q.Match(
+      q.Ref('indexes/auths_by_email')
+    ))
+  ).then((ret) => {
+    data.strava = ret
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    } 
+  })
 }
