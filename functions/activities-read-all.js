a8952fb8eccba17d92328a14c6b3c2fddb8111b2
Alan Nichol <alan@rasa.com>
2021-03-08 16:45:36 +0100

    add date filter to activities query

--- functions/activities-read-all.js
+++ functions/activities-read-all.js
@@ -1,6 +1,8 @@
 /* Import faunaDB sdk */
 const faunadb = require('faunadb')
 const q = faunadb.query
+const formatISO = require('date-fns/formatISO')
+const startOfWeek = require('date-fns/startOfWeek')
 
 
 exports.handler = (event, context) => {
@@ -9,22 +11,26 @@ exports.handler = (event, context) => {
   const client = new faunadb.Client({
     secret: process.env.FAUNADB_SERVER_SECRET
   }) 
-  return client.query(q.Paginate(q.Match(q.Ref('indexes/all_activities'))))
-    .then((response) => {
-      const activityRefs = response.data
-      console.log('Activity refs', activityRefs)
-      console.log(`${activityRefs.length} activitys found`)
-      // create new query out of activity refs. http://bit.ly/2LG3MLg
-      const getAllActivityDataQuery = activityRefs.map((ref) => {
-        return q.Get(ref)
-      })
-      // then query the refs
-      return client.query(getAllActivityDataQuery).then((ret) => {
+  const startTime = formatISO(startOfWeek(new Date(), {weekStartsOn: 1}))
+  return client.query(
+      q.Map(
+        q.Filter(
+          q.Paginate(q.Match(q.Index("all_activities_by_time"))),
+          q.Lambda(
+            ["date", "ref"],
+            q.GTE(
+              q.ToTime( q.Select(["data", "start_date"], q.Get(q.Var("ref")))),
+              q.Time(startTime)
+            )
+          )
+        ),
+        q.Lambda(["date", "ref"], q.Get(q.Var("ref")))
+      ) 
+    ).then((response) => {
         return {
           statusCode: 200,
-          body: JSON.stringify(ret)
+          body: JSON.stringify(response)
         }
-      })
     }).catch((error) => {
       console.log('error', error)
       return {
