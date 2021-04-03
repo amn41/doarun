49b9844a1fc05ffb0610a675da98c7f7c6daec3d
Alan Nichol <alan@rasa.com>
2021-04-03 19:52:37 +0200

    go back to hashrouter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AppHeader from './components/AppHeader'
 import netlifyIdentity from 'netlify-identity-widget'
 import api from './utils/api'
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Switch,
   Redirect
